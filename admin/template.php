367e8fce62e9226a4ab0be328513ea898c4581a5
hasem <hasemspi@gmail.com>
2022-04-09 09:52:08 +0600

    first commit

---
+++ admin/template.php
@@ -0,0 +1,62 @@
+<?php include("includes/header.php");?>
+  <body>
+  <body>
+	  <div class="fixed-button">
+		<a href="https://codedthemes.com/item/gradient-able-admin-template" target="_blank" class="btn btn-md btn-primary">
+			<i class="fa fa-shopping-cart" aria-hidden="true"></i> Upgrade To Pro
+		</a>
+	  </div>
+       <!-- Pre-loader start -->
+    <div class="theme-loader">
+        <div class="loader-track">
+            <div class="loader-bar"></div>
+        </div>
+    </div>
+    <!-- Pre-loader end -->
+    <div id="pcoded" class="pcoded">
+        <div class="pcoded-overlay-box"></div>
+        <div class="pcoded-container navbar-wrapper">
+
+        <?php include_once("includes/header-nav.php");?>
+
+            <div class="pcoded-main-container">
+                <div class="pcoded-wrapper">
+
+             <?php include_once("includes/sidebar-nav.php");?>
+
+                    <div class="pcoded-content">
+                        <div class="pcoded-inner-content">
+                            <div class="main-body">
+                                <div class="page-wrapper">
+
+                                    <div class="page-body">
+
+                                    <?php
+                                    if($views){
+                                        if($views === "dashboard"){
+                                            include("view/dashboard-view.php");
+                                        }elseif ($views === "add-cat"){
+                                            include ("view/add-category-view.php");
+                                        }elseif($views === "manage-cat"){
+                                            include("view/manage-category-view.php");
+                                        }elseif($views ==="add-product"){
+                                            include("view/add-product-view.php");
+                                        }elseif($views === "manage-product"){
+                                            include("view/manage-product-view.php");
+                                             
+                                        }
+                                    }
+                                    
+                                    ?>
+
+                                    <div id="styleSelector">
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+<?php include("includes/footer.php");?>
